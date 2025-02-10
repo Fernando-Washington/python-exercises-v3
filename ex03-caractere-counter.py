3bdc31b3b449c6807c39ce83b896940cfc61e053
Fernando Washington <ironitabarbosa@gmail.com>
2025-02-10 17:00:57 -0400

    ex03 finished

--- ex03-caractere-counter.py
+++ ex03-caractere-counter.py
@@ -1,3 +1,14 @@
+# Exercício 3: Contar Caracteres em uma String
+
+'''
+Crie uma função recursiva chamada contar_caracteres(s, c) que conta quantas vezes o
+caractere c aparece na string s.
+
+Exemplo de Entrada:
+
+contar_caracteres("banana", "a")
+Saída Esperada: 3
+'''
 def contar_caracteres(s: str, c: str) -> int:
     """Conta quantos caracteres 'c' existem na string 's'
 
