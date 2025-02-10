39d1af2d44db0ed748a9ab65b2df00c2a2f6d15c
Fernando Washington <ironitabarbosa@gmail.com>
2025-02-10 16:58:11 -0400

    ex02 finished

--- ex02-sum-num.py
+++ ex02-sum-num.py
@@ -14,7 +14,15 @@ Dica: Verifique se o elemento atual é uma lista ou um número para decidir se d
 continuar a recursão.
 '''
 
-def soma_lista_aninhada(lista):
+def soma_lista_aninhada(lista: list) -> int:
+    """Calcula a soma de todos os números em uma lista, mesmo que os números estejam dentro de sublistas.
+
+    Args:
+        lista (_type_): A lista poder conter números e sublistas com mais números.
+
+    Returns:
+        _type_: A soma de todos os números na lista.
+    """
     if not lista:
         return 0
     first = lista[0]
@@ -25,4 +33,12 @@ def soma_lista_aninhada(lista):
         return first + soma_lista_aninhada(lista[1:])
           
 print(soma_lista_aninhada([1, [2, 3], [4, [5]]]))
+
+'''
+Fiz um caso base para quando a lista estiver vazia, retornando 0.
+
+Verifiquei se o primeiro item da lista é uma sublista. Se for, a função chama a si mesma para somar os valores dentro dessa sublista e depois soma com o restante da lista usando slice.
+
+Caso contrário, a função soma o valor do primeiro item com a soma do resto da lista, chamando a função recursivamente até que todos os itens sejam somados.
+'''
     
